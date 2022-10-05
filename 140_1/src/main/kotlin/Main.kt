dbeb9088884e1a3860944be9104d2c275f3a4e66
Jiwan <jiwanbhandari@Jiwans-MacBook-Pro.local>
2022-10-05 12:55:05 +0545

    140

--- 140_1/src/main/kotlin/Main.kt
+++ 140_1/src/main/kotlin/Main.kt
@@ -1,8 +1,58 @@
 //https://leetcode.com/problemset/all?search=140
 //https://leetcode.com/problems/word-break-ii/
+//https://leetcode.com/problems/word-break-ii/discuss/44167/My-concise-JAVA-solution-based-on-memorized-DFS
 class Solution {
     fun wordBreak(s: String, wordDict: List<String>): List<String> {
-		println(s)
-        return wordDict
+		//return DFS(s, wordDict, hashMapOf<String, List<String>>())
+		return wordBreakImpl(s, wordDict.toSet(), hashMapOf<String, List<String>>())
     }
+
+	private fun DFS(s: String, wordDict: List<String>, m: HashMap<String, List<String>>): List<String> {
+		if(m.contains(s)) {
+			m.get(s)
+		}
+		val res = mutableListOf<String>()
+		if(s.length == 0) {
+			res.add("")
+			return res
+		}
+		for(dictionaryWord in wordDict) {
+			if(s.startsWith(dictionaryWord)) {
+				val nextList = DFS(s.substring(dictionaryWord.length), wordDict, m)
+				println("word = $dictionaryWord next list = $nextList")
+				for(nextWord in nextList) {
+					val newString = dictionaryWord + if (nextWord.isEmpty()) "" else " " + nextWord
+					res.add(newString)
+				}
+			}
+		}
+		m[s] = res
+		return res
+	}
+
+	fun wordBreakImpl(s: String, wordDict: Set<String>, m: HashMap<String, List<String>>): List<String> {
+		val res = mutableListOf<String>()
+		if (s.length == 0) {
+			return res
+		}
+		val g = m[s]
+		if (g != null) {
+			return g
+		}
+		for(i in 1..s.length) {
+			val p = s.substring(0, i)
+			if(wordDict.contains(p)) {
+				val q = s.substring(i)
+				if(q.isEmpty()) {
+					res.add(p)
+				}
+				val r = wordBreakImpl(q, wordDict, m)
+				for(w in r) {
+					res.add(p + " " + w)
+				}
+			}
+		}
+		m[s] = res
+		return res
+	}
 }
--- 140_1/src/test/kotlin/MainTest.kt
+++ 140_1/src/test/kotlin/MainTest.kt
@@ -1,4 +1,5 @@
 import org.junit.Test
+import org.junit.Ignore
 import org.junit.Assert
 
 class MainTest {
@@ -6,16 +7,18 @@ class MainTest {
 	fun `Example 1`() {
 		val s = "catsanddog"
 		val wordDict = listOf("cat","cats","and","sand","dog")
-		val expected = listOf("cats and dog","cat sand dog")
+		val expected = listOf("cat sand dog", "cats and dog",)
 		Assert.assertEquals(expected, Solution().wordBreak(s, wordDict))
 	}
+	
 	@Test
 	fun `Example 2`() {
 		val s = "pineapplepenapple"
 		val wordDict = listOf("apple","pen","applepen","pine","pineapple")
-		val expected = listOf("pine apple pen apple","pineapple pen apple","pine applepen apple")
+		val expected = listOf("pine apple pen apple","pine applepen apple","pineapple pen apple")
 		Assert.assertEquals(expected, Solution().wordBreak(s, wordDict))
 	}
+
 	@Test
 	fun `Example 3`() {
 		val s = "catsandog"
