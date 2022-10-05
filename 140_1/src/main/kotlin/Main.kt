512650e87ebc24d655aeed265bfd0945d63c100e
Jiwan <jiwanbhandari@Jiwans-MacBook-Pro.local>
2022-10-05 10:08:10 +0545

    140

--- 140_1/src/main/kotlin/Main.kt
+++ 140_1/src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
-fun main() {
-	println("Hello World!")
+//https://leetcode.com/problemset/all?search=140
+//https://leetcode.com/problems/word-break-ii/
+class Solution {
+    fun wordBreak(s: String, wordDict: List<String>): List<String> {
+		println(s)
+        return wordDict
+    }
 }
--- 140_1/src/test/kotlin/MainTest.kt
+++ 140_1/src/test/kotlin/MainTest.kt
@@ -4,8 +4,24 @@ import org.junit.Assert
 class MainTest {
 	@Test
 	fun `Example 1`() {
-		val expected = false
-		Assert.assertEquals(expected, true)
+		val s = "catsanddog"
+		val wordDict = listOf("cat","cats","and","sand","dog")
+		val expected = listOf("cats and dog","cat sand dog")
+		Assert.assertEquals(expected, Solution().wordBreak(s, wordDict))
+	}
+	@Test
+	fun `Example 2`() {
+		val s = "pineapplepenapple"
+		val wordDict = listOf("apple","pen","applepen","pine","pineapple")
+		val expected = listOf("pine apple pen apple","pineapple pen apple","pine applepen apple")
+		Assert.assertEquals(expected, Solution().wordBreak(s, wordDict))
+	}
+	@Test
+	fun `Example 3`() {
+		val s = "catsandog"
+		val wordDict = listOf("cats","dog","sand","and","cat")
+		val expected = emptyList<String>()	
+		Assert.assertEquals(expected, Solution().wordBreak(s, wordDict))
 	}
 }
 
