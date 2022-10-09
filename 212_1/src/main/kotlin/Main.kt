d7faef6272c5cd69e474b0c2e78a73aa712d96a7
Jiwan <jiwanbhandari@Jiwans-MacBook-Pro.local>
2022-10-09 16:05:56 +0545

    212

--- 212_1/src/main/kotlin/Main.kt
+++ 212_1/src/main/kotlin/Main.kt
@@ -1,12 +1,69 @@
 //https://leetcode.com/problems/word-search-ii/
 
+class TrieNode(
+    val next: Array<TrieNode?> = Array<TrieNode?>(26, { null }),
+    var word: String? = null
+)
+
 class Solution {
-    fun findWords(board: Array<CharArray>, words: Array<String>): List<String> {
-		println(board.size)
-        return words.toList()
+
+    val directions = arrayOf(
+        intArrayOf(0, 1),
+        intArrayOf(1, 0),
+        intArrayOf(-1, 0),
+        intArrayOf(0, -1)
+    )
+
+    companion object {
+        fun toIndex(c: Char): Int = c - 'a'
+        fun buildTrie(words: Array<String>): TrieNode {
+            val root = TrieNode()
+            for(word in words) {
+                var node = root
+                for(c in word) {
+                    val i = toIndex(c)
+                    val n = node.next[i] ?: TrieNode()
+                    node.next[i] = n
+                    node = n
+                }
+                node.word = word
+            }
+            return root
+        }
     }
+
+    private fun dfs(board: Array<CharArray>, i: Int, j: Int, node: TrieNode, res: MutableList<String>) {
+        val c = board[i][j]
+        if(c == '#') {
+            return
+        }
+        val idx = toIndex(c)
+        val q = node.next[idx] ?: return
+        val w = q.word
+        if (w != null) {
+            res.add(w)
+            q.word = null
+        }
+        board[i][j] = '#'
+        for(dir in directions) {
+            val x = dir.first() + i
+            val y = dir.last() + j
+            if(x >= 0 && x < board.size && y >= 0 && y < board[x].size) {
+                dfs(board, x, y, q, res)
+            }
+        }
+        board[i][j] = c
+    }
+
+    fun findWords(board: Array<CharArray>, words: Array<String>): List<String> {
+        val res = mutableListOf<String>()
+        val root = buildTrie(words)
+        for(i in 0 until board.size) {
+            for(j in 0 until board[i].size) {
+                dfs(board, i, j, root, res)
+            }
+        }
+        return res
+	}
 }
 
-fun main() {
-	println("Hello World!")
-}
--- 212_1/src/test/kotlin/MainTest.kt
+++ 212_1/src/test/kotlin/MainTest.kt
@@ -1,19 +1,30 @@
 import org.junit.Test
 import org.junit.Assert
+import org.junit.Ignore
 
 class MainTest {
+
 	@Test
 	fun `Example 1`() {
 		val board = arrayOf(charArrayOf('o','a','a','n'),charArrayOf('e','t','a','e'),charArrayOf('i','h','k','r'),charArrayOf('i','f','l','v'))
 		val words = arrayOf("oath","pea","eat","rain")
-		val expected = listOf("eat","oath")
+		val expected = listOf("oath", "eat")
 		Assert.assertEquals(expected, Solution().findWords(board, words))
 	}
+
 	@Test
 	fun `Example 2`() {
 		val board = arrayOf(charArrayOf('a','b'),charArrayOf('c','d'))
 		val words = arrayOf("abcb")
 		val expected = listOf<String>()
+		Assert.assertEquals(expected, Solution().findWords(board, words))
+	}
+
+	@Test
+	fun `Example 3`() {
+		val board = arrayOf(charArrayOf('o','a','b','n'),charArrayOf('o','t','a','e'),charArrayOf('a','h','k','r'),charArrayOf('a','f','l','v'))
+		val words = arrayOf("oa","oaa")
+		val expected = listOf("oa","oaa")
 		Assert.assertEquals(expected, Solution().findWords(board, words))
 	}
 }
